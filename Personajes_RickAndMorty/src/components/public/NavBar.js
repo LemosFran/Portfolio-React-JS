3adcd1d1ee0ee9979381c72672a338558b842657
Franco <you@example.com>
2023-09-30 12:10:07 -0300

    proyecto react js y bootstrap

--- Personajes_RickAndMorty/src/components/public/NavBar.js
+++ Personajes_RickAndMorty/src/components/public/NavBar.js
@@ -25,9 +25,9 @@ const NavBar = () => {
             </li>
           </ul>
 
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
             <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-          </form>
+          </form> */}
 
         
            <div className="text-end">
